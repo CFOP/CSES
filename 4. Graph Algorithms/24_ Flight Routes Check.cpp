d845b76dd1c40b809b28204633e94daaa0ea58bb
Yoguel Salazar <yoguel.salazar@gmail.com>
2020-09-24 19:12:45 -0500

    4.24

--- 4. Graph Algorithms/24_ Flight Routes Check.cpp
+++ 4. Graph Algorithms/24_ Flight Routes Check.cpp
@@ -7,48 +7,19 @@ typedef vector<int> vi;
 typedef long long ull;
 typedef pair<ull, pair<int, int>> pii;
 
-struct UF{
-    vi e;
-    UF(int n) : e(n, -1) {}
-    bool sameSet(int a, int b) {return find(a) == find(b);}
-    int size(int x) {return -e[find(x)];}
-    int find(int x) {return e[x] < 0 ? x : e[x] = find(e[x]);}
-    bool join(int a, int b){
-        a = find(a), b = find(b);
-        if(a==b) return false;
-        if(e[a]>e[b]) swap(a, b);
-        e[a]+=e[b]; e[b] = a;
-        return true;
-    }
-};
-
-UF uf(MAX);
-
 vi adj[MAX];
+vi inv[MAX];
 int visited[MAX];
 
-int dfs(int v, int p = 0){
-    if(visited[v]){
-        if(visited[v]==1)
-            return v;
-        if(visited[v] == 2) {
-            cout<<"NO\n";
-            cout<<v<<" "<<p<<"\n";
-            exit(0);
-        }
+void dfs(vi *adj, int s, int v, int p = 0){
+    if(visited[v]>=s)
         return;
-    }
-    visited[v] = 1;
+    visited[v]+=s;
 
     int ans = 0;
-    for(int u : adj[v]){
-        dfs(u, v);
-        uf.join(u, v);
-    }
+    for(int u : adj[v])
+        dfs(adj, s, u, v);
 
-    visited[v] = 2;
-
-    return r;
 }
 
 int main(){
@@ -60,17 +31,19 @@ int main(){
     for(int i=0; i<m; i++){
         int a, b; cin>>a>>b;
         adj[a].push_back(b);
+        inv[b].push_back(a);
     }
 
-    dfs(1);
+    dfs(adj, 1, 1);
+    dfs(inv, 2, 1);
 
     for(int i=1; i<=n; i++)
-        if(!visited[i]){
+        if(!visited[i] || visited[i]==2){
             cout<<"NO\n";
             cout<<1<<" "<<i<<"\n";
             return 0;
         }
-        else if(!uf.sameSet(1, i)){
+        else if(visited[i]==1){
             cout<<"NO\n";
             cout<<i<<" "<<1<<"\n";
             return 0;
