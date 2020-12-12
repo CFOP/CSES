4c8e5dfa0ea3cd552de34cc0c1a639074e161a99
cfop2357 <yoguel.salazar@gmail.com>
2020-12-11 18:25:45 -0600

    5.11

--- 5. Range Queries/11_ Subarray Sum Queries.cpp
+++ 5. Range Queries/11_ Subarray Sum Queries.cpp
@@ -11,33 +11,66 @@ struct Range{
     ull max_range;
 
     Range() : min_sum(0), max_sum(0), sum(0), max_range(0) {}
-    Range(ull value) : sum(value), min_sum(min((ull)0, value)), max_sum(max((ull)0, value)), max_range(max(value, (ull)0)){}
+    Range(ull value) : sum(value), min_sum(value), max_sum(value), max_range(value){}
     Range(ull sum, ull min_sum, ull max_sum, ull max_range) : sum(sum), min_sum(min_sum), max_sum(max_sum), max_range(max_range){}
 };
 
 inline Range join_range(Range a, Range b){
-    return Range(a.sum + b.sum, min(a.min_sum, b.min_sum+a.sum), min(a.max_sum, b.max_sum+a.sum), max( {a.max_range, b.max_range, b.max_sum + a.sum - a.min_sum} ) );
+    return Range(
+                 a.sum + b.sum, //sum
+                 min(a.min_sum, b.min_sum+a.sum), //min_sum
+                 max(a.max_sum, b.max_sum+a.sum), //max_sum
+                 max( {a.max_range, b.max_range, b.max_sum + a.sum - a.min_sum} ) //max_range
+    );
 }
 
-struct Tree {
-	typedef Range T;
-	T f(T a, T b) { return join_range(a, b); } // (any associative fn)
-	vector<T> s; int n;
+template<typename T = Range> struct ST{
+    int l, r;
+    T acum;
 
-	Tree(int n = 0) : s(2*n, T()), n(n) {}
+    ST *left, *right;
 
-	void update(int pos, T val) {
-		for (s[pos += n] = val; pos /= 2;)
-			s[pos] = f(s[pos * 2], s[pos * 2 + 1]);
-	}
-	T query(int b, int e) { // query [b, e)
-		T ra = T(), rb = T();
-		for (b += n, e += n; b < e; b /= 2, e /= 2) {
-			if (b % 2) ra = f(ra, s[b++]);
-			if (e % 2) rb = f(s[--e], rb);
-		}
-		return f(ra, rb);
-	}
+    T none = Range();
+
+    T operation(T a, T b){
+        return join_range(a, b);
+    }
+
+    ST(int l, int r, vector<T> &arr): l(l), r(r){
+        if(l >= arr.size())
+            acum = none;
+        else if(l==r)
+            acum = arr[r];
+        else {
+            int m = (l+r)/2;
+            left = new ST(l, m, arr);
+            right = new ST(m+1, r, arr);
+            acum = operation(left->acum,  right->acum);
+        }
+    }
+
+    T get(int l, int r){
+        if(this->l > r || this->r < l)
+            return none;
+        //cout<<this->l<<" "<<this->r<<" "<<this->acum<<"\n";
+        if(this->l >= l && this->r <= r)
+            return acum;
+        int m = (l+r)/2;
+        return operation(left->get(l, r), right->get(l, r));
+    }
+
+    T update(int i, T v){
+        if(i > r || i < l)
+            return acum;
+
+        if(i>=l && i<=r){
+            if(l==r)
+                return acum = v;
+            acum = operation(left->update(i, v), right->update(i, v));
+        }
+        return acum;
+    }
+
 };
 
 int main(){
@@ -46,22 +79,25 @@ int main(){
     int n, m;
     cin>>n>>m;
 
-    Tree st(n+1);
-
+    vector<Range> v;
+    v.push_back(Range());
     for(int i=1; i<=n; i++){
         ull x; cin>>x;
-        st.update(i, Range(x));
+        v.push_back(Range(x));
     }
 
-    for(int i=1; i+1<=n; i++)
-        cout<<st.query(i, i+2).max_sum<<" ";
-    cout<<"#\n";
+    ST<Range> st(0, n, v);
+
+    /*const int sz = 1;
+    for(int i=1; i+sz-1<=n; i++)
+        cout<<st.get(i, i+sz-1).max_sum<<" ";
+    cout<<"#\n";*/
 
     while(m--){
         ull k, x; cin>>k>>x;
         st.update(k, Range(x));
 
-        cout<<st.query(1, n+1).max_range<<"\n";
+        cout<<st.get(0, n).max_range<<"\n";
     }
 
     return 0;
